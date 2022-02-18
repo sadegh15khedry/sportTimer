087722fd1ab0f8651d9ca8e0c67c8f264dd9fe9c
sadegh15khedry <sadegh15khedry@gmail.com>
2022-02-18 16:00:05 +0330

    fragments added

--- app/src/main/java/com/example/sporttimer/MainActivity.java
+++ app/src/main/java/com/example/sporttimer/MainActivity.java
@@ -5,10 +5,12 @@ import android.os.Bundle;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.snackbar.Snackbar;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.view.View;
 
+import androidx.fragment.app.Fragment;
 import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 import androidx.navigation.ui.AppBarConfiguration;
@@ -22,42 +24,48 @@ import android.view.MenuItem;
 public class MainActivity extends AppCompatActivity {
 
     private AppBarConfiguration appBarConfiguration;
-    private ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
 
-        binding = ActivityMainBinding.inflate(getLayoutInflater());
-        setContentView(binding.getRoot());
-
-        //setSupportActionBar(binding.toolbar);
-
-
+        //setting bottom navigation
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
+        bottomNavigationView.setOnItemSelectedListener(bottomNavigationItemSelectedListener);
 
     }
 
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_main, menu);
-        return true;
-    }
 
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
 
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+
+
+    //bottom nav item select
+    final private BottomNavigationView.OnItemSelectedListener bottomNavigationItemSelectedListener = new BottomNavigationView.OnItemSelectedListener() {
+        @Override
+        public boolean onNavigationItemSelected(@NonNull MenuItem item) {
+            Fragment selectedFragment = null;
+
+            if (item.getItemId() == R.id.nav_moves) {
+                selectedFragment = new MovesFragment();
+
+            } else if (item.getItemId() == R.id.nav_routine) {
+                selectedFragment = new RoutinesFragment();
+
+            }
+            else if (item.getItemId() == R.id.nav_add) {
+                selectedFragment = new AddFragment();
+            }
+            else if (item.getItemId() == R.id.nav_quick) {
+                selectedFragment = new QuickFragment();
+            }
+
+            getSupportFragmentManager().beginTransaction().replace(R.id.fragment_container,
+                    selectedFragment).commit();
             return true;
         }
 
-        return super.onOptionsItemSelected(item);
-    }
 
-
+    };
 }
