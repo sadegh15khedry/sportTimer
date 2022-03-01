3021744a56d5674245324eb24b7fd56e54d01fb8
sadegh <sadegh15khedry@gmail.com>
2022-03-01 15:30:31 +0330

    sec countdown added

--- app/src/main/java/com/example/sporttimer/TimerActivity.java
+++ app/src/main/java/com/example/sporttimer/TimerActivity.java
@@ -2,12 +2,19 @@ package com.example.sporttimer;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.os.CountDownTimer;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class TimerActivity extends AppCompatActivity {
+import java.util.Timer;
+import java.util.TimerTask;
 
+public class TimerActivity extends AppCompatActivity {
+    private CountDownTimer countDownTimer;
+    private long timeLeftInMilliSeconds;
+    private TextView timerTimeRemainingTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -17,13 +24,40 @@ public class TimerActivity extends AppCompatActivity {
         String work = getIntent().getStringExtra("work");
         String rest = getIntent().getStringExtra("rest");
 
+        timeLeftInMilliSeconds = Integer.parseInt(work);
+        timeLeftInMilliSeconds = timeLeftInMilliSeconds * 1000;
+
         TextView setsTextView = findViewById(R.id.timerSetsTextView);
-        TextView timerTimeRemainingTextView = findViewById(R.id.timerTimeRemainingTextView);
+        timerTimeRemainingTextView = findViewById(R.id.timerTimeRemainingTextView);
 
         setsTextView.setText(sets);
         timerTimeRemainingTextView.setText(work);
+        startTimer();
 
-//        Toast.makeText(this,sets,
-//                Toast.LENGTH_SHORT).show();
+        setsTextView.setText(sets);
+
     }
+
+
+    private void startTimer(){
+        countDownTimer = new CountDownTimer(timeLeftInMilliSeconds, 1000) {
+            @Override
+            public void onTick(long newtTimeLeftInMilliSeconds) {
+//                timeLeftInMilliSeconds = newtTimeLeftInMilliSeconds;
+//                timerTimeRemainingTextView.setText(String.valueOf(timeLeftInMilliSeconds/1000));
+                timeLeftInMilliSeconds = timeLeftInMilliSeconds - 1000;
+
+//                Toast.makeText(getApplicationContext(),"working",
+//                Toast.LENGTH_SHORT).show();
+                timerTimeRemainingTextView.setText(String.valueOf(timeLeftInMilliSeconds/1000));
+
+            }
+
+            @Override
+            public void onFinish() { }
+
+        }.start();
+    }
+
+    private void pauseTimer(){}
 }
